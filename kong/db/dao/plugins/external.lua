952a49463e4f9d197c00231aadaf2135c8f259e7
Javier Guerra <javier.guerra@konghq.com>
2020-10-27 09:16:46 -0500

    docs and ideas

--- kong/db/dao/plugins/external.lua
+++ kong/db/dao/plugins/external.lua
@@ -12,6 +12,72 @@ local _plugin_infos
 
 
 
+--[[
+
+Configuration
+
+The external_plugins_config YAML file defines a list of plugin servers.  Each one
+can have the following fields:
+
+name: (required) a unique string.  Shouldn't collide with Lua packages available as `kong.plugins.<name>`
+socket: (required) path of a unix domain socket to use for RPC.
+exec: (optional) executable file of the server.  If omitted, the server process won't be managed by Kong.
+args: (optional) a list of strings to be passed as command line arguments.
+env: (optional) a {string:string} map to be passed as environment varialbes.
+info_cmd: (optional) command line to request plugin info.
+
+--]]
+
+--[[
+
+RPC
+
+Each plugin server specifies a socket path to communicate.  Protocol is the same
+as Go plugins.
+
+CONSIDER:
+
+- when a plugin server notifies a new PID, Kong should request all plugins info again.
+  Should it use RPC at this time, instead of commandline?
+
+- Should we add a new notification to ask kong to request plugin info again?
+
+--]]
+
+--[[
+
+Plugin info requests
+
+Disclaimer:  The best way to do it is to have "ListPlugins()" and "GetInfo(plugin)"
+RPC methods; but Kong would like to have all the plugin schemas at initialization time,
+before full cosocket is available.  At one time, we used blocking I/O to do RPC at
+non-yielding phases, but was considered dangerous.  The alternative is to use
+`io.popen(cmd)` to ask fot that info.
+
+
+In the externel plugins configuration, the `.info_cmd` field contains a string
+to be executed as a command line.  The output should be a JSON string that decodes
+as an array of objects, each defining the name, priority, version and schema of one
+plugin.
+
+    [{
+      "name": ... ,
+      "priority": ... ,
+      "version": ... ,
+      "schema": ... ,
+    },
+    {
+      ...
+    },
+    ...
+    ]
+
+This array should describe all plugins currently available through this server,
+no matter if actually enabled in Kong's configuration or not.
+
+--]]
+
+
 local function register_plugin_info(server_def, plugin_info)
   if _plugin_infos[plugin_info.name] then
     kong.log.error(string.format("Duplicate plugin name [%s] by %s and %s",
@@ -66,7 +132,7 @@ local function load_all_infos()
     local conf = lyaml.load(assert(pl_file.read(kong.configuration.external_plugins_config)))
     _plugin_infos = {}
 
-    for i, server_def in ipairs(conf) do
+    for _, server_def in ipairs(conf) do
       ask_info(server_def)
     end
   end
@@ -84,6 +150,18 @@ function external_plugins.load_schema(plugin_name)
   return plugin_info and plugin_info.schema
 end
 
+
+--[[
+
+Process management
+
+Servers that specify an `.exec` field are launched and managed by Kong.
+This is an attempt to duplicate the smallest reasonable subset of systemd.
+
+Each process specifies executable, arguments and environment.  If it dies,
+Kong logs the event and respawns it.
+
+--]]
 
 local function handle_server(server_def)
   if not server_def.socket then
